60d2278b55362a92314d131a10c385d943437322
Omar Real7 <cc114304@gmail.com>
2016-04-05 14:13:19 +0300

    RealBot v1.0

---
+++ plugins/echo.lua
@@ -0,0 +1,29 @@
+--[[
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY OmarReal                       ▀▄ ▄▀ 
+▀▄ ▄▀     BY OmarReal (Omar_Real7)         ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY OmarReal              ▀▄ ▄▀   
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+
+local function run(msg, matches)
+  local text = matches[1]
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  return text
+end
+
+return {
+  description = "Simplest plugin ever!",
+  usage = "!echo [whatever]: echoes the msg",
+  patterns = {
+    "^/echo +(.+)$"
+  }, 
+  run = run 
+}
